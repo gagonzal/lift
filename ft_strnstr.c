f3480f2dd45a88904e624948e53ee8cf8cbdf906
haris <hkidds23@gmail.com@>
2017-11-19 23:03:17 +0100

    libft de meeeeeeeeeeerde #allezLOM

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,19 +11,34 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include "string.h"
 
 char	*ft_strnstr(const char *s1, const char *s2, size_t len)
 {
-	int i;
+		size_t i;
 
-	i = 0;
-	if (s2[i] == '\0')
-		return (char*)(s1);
-	while (s1[i])
-	{
-		if ((ft_strncmp(s1 + i, s2, len)) == 0)
-			return (char*)(s1 + i);
-		i++;
-	}
-	return (NULL);
+		i = 0;
+		if (s2[i] == '\0')
+				return (char*)(s1);
+		while (s1[i] &&	i < len)
+		{
+				if ((ft_strncmp(s1 + i, s2, ft_strlen(s2)) == 0))
+						return (char*)(s1 + i);
+				i++;
+		}
+		return (NULL);
+}
+
+int		main(void)
+{
+
+		char    *s2 = "see F your FF return FF now FF";
+		char    *s1 = "FF";
+		size_t  max = strlen(s1) + 1;
+
+		char    *i1 = strnstr(s1, s2, max);
+		char    *i2 = ft_strnstr(s1, s2, max);
+
+		printf("{%s},%zu\n", i1, max);
+		printf("[%s]", i2);
 }
